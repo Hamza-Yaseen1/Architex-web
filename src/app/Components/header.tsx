864e927786a72337c03a24d356de7a739c4628ab
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-10-20 13:24:25 +0500

    Header Created

---
+++ src/app/Components/header.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { useState } from 'react';
+import './header.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <section className="navbar flex justify-between items-center">
+      <div className="logo pl-4 pt-5">
+        <Image
+          src="/images/Logo.png"
+          alt="Logo"
+          width={69}
+          height={42}
+        />
+      </div>
+      <div className="burger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className={`nav-items ${isOpen ? 'active' : ''}`}>
+        <ul className="flex justify-between pr-8 pt-5 gap-9">
+          <Link href="/about"><li>MAIN</li></Link>
+          <Link href="/gallery"><li>GALLERY</li></Link>
+          <Link href="/projects"><li>PROJECTS</li></Link>
+          <Link href="/certifications"><li>CERTIFICATIONS</li></Link>
+          <Link href="/contact"><li>CONTACT US</li></Link>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Header from "./Components/header";
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
