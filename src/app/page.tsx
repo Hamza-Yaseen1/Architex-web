c8681c2c2d9d9b19e9d2038e63e7464faffe2373
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2025-02-07 16:37:35 +0500

    Remove Seo component and update metadata in layout for improved SEO and branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,16 @@ import About from "./Components/about";
 import Focus from "./Components/focus";
 import Hero from "./Components/hero"
 import Projects from "./Components/project";
-import Seo from "./Components/Seo";
 export default function Home() {
   return (
     <>
-      <Hero />
-      <About />
-      <Focus />
-      <Projects />
-      <Seo 
-      title="Best Land Deals in Pakistan | LandHub" 
-      description="Explore the best land deals in Karachi, Lahore, Islamabad & all over Pakistan. Secure your future with prime land investments!" 
-      />
+    <Hero/>
+    <About/>
+    <Focus/>
+    {/* <br /> */}
+    {/* <br /> */}
+    <Projects/>
+    
     </>
   );
 }
