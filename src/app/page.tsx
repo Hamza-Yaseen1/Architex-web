15c0069a7d394a1881a96d809f68dddfae504bd4
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2025-02-07 16:26:54 +0500

    Add SEO component and integrate it into the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,18 @@ import About from "./Components/about";
 import Focus from "./Components/focus";
 import Hero from "./Components/hero"
 import Projects from "./Components/project";
+import Seo from "./Components/Seo";
 export default function Home() {
   return (
     <>
-    <Hero/>
-    <About/>
-    <Focus/>
-    {/* <br /> */}
-    {/* <br /> */}
-    <Projects/>
+      <Hero />
+      <About />
+      <Focus />
+      <Projects />
+      <Seo 
+      title="Best Land Deals in Pakistan | LandHub" 
+      description="Explore the best land deals in Karachi, Lahore, Islamabad & all over Pakistan. Secure your future with prime land investments!" 
+      />
     </>
   );
 }
