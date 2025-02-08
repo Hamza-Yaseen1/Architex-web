33a666f18da60be08d411b3b94515426575457e9
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2025-02-08 20:12:20 +0500

    Adjust spacing and layout in Focus and Projects components

--- src/app/Components/focus.tsx
+++ src/app/Components/focus.tsx
@@ -25,6 +25,8 @@ function Focus() {
         </div>
         
       </section>
+      <br /><br />
+      <br />
     </div>
   );
 }
--- src/app/Components/project.tsx
+++ src/app/Components/project.tsx
@@ -5,10 +5,11 @@ import Button from "./button";
 function Projects() {
   return (
     <section className="projects p-4 md:p-8">
-      <h1 className="text-3xl md:text-5xl text-gray-700 pt-6 md:pt-9 pl-2 md:pl-12 font-light text-center md:text-left">
+      <h1 className="text-3xl md:text-5xl text-gray-700 pt-6   md:pt-9 pl-2 md:pl-12 font-light text-center md:text-left">
         Our Projects
       </h1>
       <section className="project-images space-y-6 md:space-y-0">
+
         
         {/* First Row */}
         <div className="line-1 h-80 flex flex-col md:flex-row justify-around pt-6 md:pt-9 gap-4">
